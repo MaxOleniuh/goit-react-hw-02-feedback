b3eb6a344887a9c088493b59fda3713f1743c074
oleniuh <maxim.oleniukh@gmail.com>
2023-04-26 23:32:43 +0100

    statistics propTypes completed

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { StatsContainer, TitleStyled, SpanStyled } from "./Statistics.styled";
 const Statistics = ({ good, neutral, bad, total, positivePercentage, title }) => {
   return (
@@ -12,5 +13,12 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage, title }) =>
     </StatsContainer>
   );
 };
-
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  title: PropTypes.string.isRequired
+}
 export default Statistics;
